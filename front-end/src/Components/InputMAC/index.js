4efa17b0d2497cb1f171b944181aeb23c16c545e
democrito88 <d_who@hotmail.com>
2024-01-31 13:32:48 -0300

    31-01-2024

--- front-end/src/Components/InputMAC/index.js
+++ front-end/src/Components/InputMAC/index.js
@@ -2,10 +2,17 @@ import { useState } from "react";
 import { Form } from "react-bootstrap";
 
 function formatMAC(value) {
-    const parts = [];
+    let parts = [];
+    value = value.split(":").join('');
+
     for (let i = 0; i < value.length; i += 2) {
         parts.push(value.slice(i, i + 2));
     }
+
+    if (parts.length > 6) {
+        parts = parts.slice(0, 6);
+    }
+
     return parts.join(':');
 }
 
