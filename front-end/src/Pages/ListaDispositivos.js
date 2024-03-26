c7bd92dd722b1a16bbcdde951fb5250ff00defb6
democrito88 <d_who@hotmail.com>
2024-03-26 15:49:20 -0300

    26-03-2024

--- front-end/src/Pages/ListaDispositivos.js
+++ front-end/src/Pages/ListaDispositivos.js
@@ -1,20 +1,28 @@
-import { Container } from "react-bootstrap";
+import { Container, Form} from "react-bootstrap";
 import TabelaComputadores from "../Components/TabelaComputadores";
+import { useState } from "react";
 
 
-export default function ListaDispositivos({computadores, funcionarios, setComputadores, setFuncionarios}){
-    
+export default function ListaDispositivos({ computadores, funcionarios, setComputadores, setFuncionarios }) {
+  const [computadoresAExibir, setComputadoresAExibir] = useState(computadores);
+  
+  const handleBusca = (e) => {
+     setComputadoresAExibir(computadores.filter(computador => computador.nome === e.target));
+  }
 
-    return(
-        <Container>
-        {computadores.length > 0 ? (
-          <TabelaComputadores
-            computadores={computadores}
-            funcionarios={funcionarios}
-          />
-        ) : (
-          <p>Ainda não foi cadastrado nenhum dispositivo</p>
-        )}
-      </Container>
-    );
+  return (
+    <Container>
+      <Form>
+        <Form.Control type="text" placeholder="busque pelo nome" onInput={handleBusca}/>
+      </Form>
+      {computadores.length > 0 ? (
+        <TabelaComputadores
+          computadores={computadores}
+          funcionarios={funcionarios}
+        />
+      ) : (
+        <p>Ainda não foi cadastrado nenhum dispositivo</p>
+      )}
+    </Container>
+  );
 }
