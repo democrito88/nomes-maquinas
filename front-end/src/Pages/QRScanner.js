f1f9c54b9d4d339a7301d45678cc1f92675b253e
democrito88 <d_who@hotmail.com>
2024-03-26 12:32:55 -0300

    26-03-2024

--- front-end/src/Pages/QRScanner.js
+++ front-end/src/Pages/QRScanner.js
@@ -1,14 +1,16 @@
-import { Stack } from "react-bootstrap";
-import Scanner from "../Components/Scanner";
-import WebcamCapture from "../Components/WebcamCapture";
+//import { Stack } from "react-bootstrap";
+//import Scanner from "../Components/Scanner";
+//import WebcamCapture from "../Components/WebcamCapture";
 
 export default function QRScanner() {
 
-
-    return (
+    return(
+        <></>
+    );
+    /*return (
         <Stack gap={3}>
             <Scanner />
             <WebcamCapture />
         </Stack>
-    );
+    );*/
 }
