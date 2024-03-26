d95be91f2d1f1cabf9d577201fa7c642d58293e5
democrito88 <d_who@hotmail.com>
2024-03-26 14:40:33 -0300

    26-03-2024

--- back-end/inicializador.js
+++ back-end/inicializador.js
@@ -7,50 +7,56 @@ function populateTableIfEmpty(tableName, jsonFilePath) {
     const data = fs.readFileSync(jsonFilePath, "utf8");
 
     // Check if the table is already populated
-    const rows = db.all(`SELECT COUNT(*) FROM ${tableName}`);
-    if (Object.keys(rows).length === 0) {
-      // Parse the JSON content
-      const jsonData = JSON.parse(data);
-      let queryValues = "";
-
-      jsonData.forEach((item) => {
-        // Adjust the code based on the structure of your JSON and database schema
-        if (tableName === "secretarias") {
-          queryValues += `('${item.nome}', '${item.sigla}', '${item.codigo}'),`;
-        } else if (tableName === "funcionarios") {
-          queryValues += `('${item.nome}', '${item.setor_id}', '${item.funcao}', '${item.cargo}'),`;
-        } else {
-          queryValues += `('${item.nome}', '${item.sigla}', '${item.codigo}', '${item.idSecretaria}'),`;
-        }
-      });
-
-      console.log(`Populating table '${tableName}'.\n`);
-
-      // Execute the INSERT query
-      if (tableName === "secretarias") {
-        db.run(`
-          INSERT INTO ${tableName} (nome, sigla, codigo) VALUES
-          ${queryValues}
-          ('', '', 1);
-        `);
-      } else if (tableName === "funcionarios") {
-        db.run(`
-        INSERT INTO ${tableName} (nome, setor_id, funcao, cargo) VALUES
-        ${queryValues}
-        ('', 1, '', '');
-      `);
-      } else {
-        db.run(`
-          INSERT INTO ${tableName} (nome, sigla, codigo, secretaria_id) VALUES
-          ${queryValues}
-          ('', '', 1, 1);
-        `);
+    db.get(`SELECT COUNT(*) AS qtde FROM ${tableName}`, (error, row) =>{
+      if(error){
+        console.error(`Não foi possível popular a tabela ${tableName}`);
+        return;
       }
 
-      console.log(`Table '${tableName}' populated.\n`);
-    } else {
-      console.log(`Table '${tableName}' already populated.\n`);
-    }
+      if (row.qtde == 0) {
+        // Parse the JSON content
+        const jsonData = JSON.parse(data);
+        let queryValues = "";
+  
+        jsonData.forEach((item) => {
+          // Adjust the code based on the structure of your JSON and database schema
+          if (tableName === "secretarias") {
+            queryValues += `('${item.nome}', '${item.sigla}', '${item.codigo}'),`;
+          } else if (tableName === "funcionarios") {
+            queryValues += `('${item.nome}', '${item.setor_id}', '${item.funcao}', '${item.cargo}'),`;
+          } else {
+            queryValues += `('${item.nome}', '${item.sigla}', '${item.codigo}', '${item.idSecretaria}'),`;
+          }
+        });
+  
+        console.log(`Populating table '${tableName}'.\n`);
+  
+        // Execute the INSERT query
+        if (tableName === "secretarias") {
+          db.run(`
+            INSERT INTO ${tableName} (nome, sigla, codigo) VALUES
+            ${queryValues}
+            ('', '', 1);
+          `);
+        } else if (tableName === "funcionarios") {
+          db.run(`
+          INSERT INTO ${tableName} (nome, setor_id, funcao, cargo) VALUES
+          ${queryValues}
+          ('', 1, '', '');
+        `);
+        } else {
+          db.run(`
+            INSERT INTO ${tableName} (nome, sigla, codigo, secretaria_id) VALUES
+            ${queryValues}
+            ('', '', 1, 1);
+          `);
+        }
+  
+        console.log(`Table '${tableName}' populated.\n`);
+      } else {
+        console.log(`Table '${tableName}' already populated.\n`);
+      }
+    });
   } catch (error) {
     console.error(`Error populating table '${tableName}':`, error);
   }
